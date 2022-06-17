c6949613651171fba0e7f9e8625b886da91665fc
Marcos França <marcosfranca.vani@gmail.com>
2022-06-16 21:03:05 -0300

    Ajustes gerais (no js)

--- calc.js
+++ calc.js
@@ -1,5 +1,17 @@
 
-function insert(num){
+var operador = ''
+function insert(num) {
+    var reg = /^\d*\.?\d*$/;
+    var display = document.getElementById('result').innerHTML
+    if (display.length == 0 && !reg.test(num)) {
+        return;
+    }
+    if ((display.includes('+') || display.includes('-')) && !reg.test(num)) {
+        if (!reg.test(display.slice(display.length - 1))) {
+            return;
+        }
+        calc()
+    }
     var number = document.getElementById("result").innerHTML;
     document.getElementById('result').innerHTML = number + num;
     var v1 = number.includes(`+-`)
@@ -21,70 +33,71 @@ function insert(num){
     var g2 = number.includes(`/*`)
     var g3 = number.includes(`//`)
     var g4 = number.includes(`/+`)
-    if (v1 == true){
+    if (v1 == true) {
         n1 = number.replace(`+-`, `-`)
-         window.document.getElementById(`result`).innerHTML = n1 + num
-    
-    }else if (v2 == true){
+        window.document.getElementById(`result`).innerHTML = n1 + num
+
+    } else if (v2 == true) {
         n1 = number.replace(`+*`, `*`)
-         window.document.getElementById(`result`).innerHTML = n1 + num
-        }else if (v3 == true){
-            n1 = number.replace(`+/`, `/`)
-             window.document.getElementById(`result`).innerHTML = n1 + num
-            }else if (v4 == true){
-                n1 = number.replace(`++`, `+`)
-                 window.document.getElementById(`result`).innerHTML = n1 + num
- }else if (b1 == true){
-    n1 = number.replace(`--`, `-`)
-    window.document.getElementById(`result`).innerHTML = n1 + num
-                
-    }else if (b2 == true){
-         n1 = number.replace(`-*`, `*`)
-         window.document.getElementById(`result`).innerHTML = n1 + num
-            
-        }else if (b3 == true){
-             n1 = number.replace(`-/`, `/`)
-             window.document.getElementById(`result`).innerHTML = n1 + num
-            }else if (b4 == true){
-                n1 = number.replace(`-+`, `+`)
-                 window.document.getElementById(`result`).innerHTML = n1 + num
-}else if (f1 == true){
-    n1 = number.replace(`*-`, `-`)
-     window.document.getElementById(`result`).innerHTML = n1 + num
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (v3 == true) {
+        n1 = number.replace(`+/`, `/`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (v4 == true) {
+        n1 = number.replace(`++`, `+`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (b1 == true) {
+        n1 = number.replace(`--`, `-`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
 
-    }else if (f2 == true){
-    n1 = number.replace(`**`, `*`)
-     window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (b2 == true) {
+        n1 = number.replace(`-*`, `*`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
 
-        }else if (f3 == true){
+    } else if (b3 == true) {
+        n1 = number.replace(`-/`, `/`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (b4 == true) {
+        n1 = number.replace(`-+`, `+`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (f1 == true) {
+        n1 = number.replace(`*-`, `-`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+
+    } else if (f2 == true) {
+        n1 = number.replace(`**`, `*`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+
+    } else if (f3 == true) {
         n1 = number.replace(`*/`, `/`)
-         window.document.getElementById(`result`).innerHTML = n1 + num
-            }else if (f4 == true){
-            n1 = number.replace(`*+`, `+`)
-             window.document.getElementById(`result`).innerHTML = n1 + num
-}else if (g1 == true){
-    n1 = number.replace(`/-`, `-`)
-     window.document.getElementById(`result`).innerHTML = n1 + num
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (f4 == true) {
+        n1 = number.replace(`*+`, `+`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (g1 == true) {
+        n1 = number.replace(`/-`, `-`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
 
-    }else if (g2 == true){
-    n1 = number.replace(`/*`, `*`)
-     window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (g2 == true) {
+        n1 = number.replace(`/*`, `*`)
+        window.document.getElementById(`result`).innerHTML = n1 + num
 
-        }else if (g3 == true){
+    } else if (g3 == true) {
         n1 = number.replace(`//`, `/`)
-         window.document.getElementById(`result`).innerHTML = n1 + num
-            }else if (g4 == true){
-            n1 = number.replace(`/+`, `+`)
-             window.document.getElementById(`result`).innerHTML = n1} + num
+        window.document.getElementById(`result`).innerHTML = n1 + num
+    } else if (g4 == true) {
+        n1 = number.replace(`/+`, `+`)
+        window.document.getElementById(`result`).innerHTML = n1
+    } + num
 }
 
-function clean(){
+function clean() {
     document.getElementById('result').innerHTML = "";
 }
 
-function calc(){
+function calc() {
     var result = document.getElementById("result").innerHTML;
-        if(result){
-            document.getElementById('result').innerHTML = eval(result);
-        }
+    if (result) {
+        document.getElementById('result').innerHTML = eval(result);
     }
+}
